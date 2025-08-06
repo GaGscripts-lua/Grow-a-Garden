e66be1f0661a3949d981abd371b7bba3174342de
GaGscripts-lua <mr.payatot441@gmail.com>
2025-08-06 20:24:06 +0800

    Update Trade-Freeze.lua

--- Trade-Freeze.lua
+++ Trade-Freeze.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/QzYpowK1/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GaGscripts-lua/Grow-a-Garden/refs/heads/main/Trade-Freeze.lua"))()
